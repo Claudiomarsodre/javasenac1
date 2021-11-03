48b58e35489d5619271683508e6074bb071a0eda
usuario <usuario@usuario-PC>
2021-11-03 20:29:23 -0300

    Primeiro Programa

---
+++ src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primeiroprograma;
+
+/**
+ *
+ * @author usuario
+ */
+public class PrimeiroPrograma {
+    
+    
+    public static void main(String[] args) {
+        
+        //mostra a mensagem "Meu primeiro programa em java"
+        System.out.println ("Meu primerio programa java");
+        
+    }
+    
+}
