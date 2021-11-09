82f5b81708ea1d0d73aaaf342f98795d4f3179eb
usuario <usuario@usuario-PC>
2021-11-08 21:25:57 -0300

    Mini calculadora com dois números mostrando o resultado da soma, subtração, multiplicação e divisão

---
+++ IfeElse/src/ifeelse/IfeElse.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ifeelse;
+
+public class IfeElse {
+
+    public static void main(String[] args) {
+  int idade = 85;
+  
+  if (idade <= 40)
+  { 
+      System.out.println("Menor que 40");
+  }
+  else if (idade <= 60)
+  {
+      System.out.println("Menor que 60");
+  }
+  else if (idade <=80)
+  {
+      System.out.println("Menor que 80");
+  }
+  else if (idade <= 100)
+            System.out.println("menor que 100");
+
+      
+  }
+  }
+    }
+    
+
