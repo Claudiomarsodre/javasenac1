f3d1213f3119eb5630dccf1c37dcc0c0ed619578
usuario <usuario@usuario-PC>
2021-11-11 20:09:41 -0300

    IncleDecre

---
+++ Inclemento/src/inclemento/Inclemento.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package inclemento;
+
+public class Inclemento {
+
+    public static void main(String[] args) {
+  int a=1;
+  int b=1;
+        System.out.println("Valor inicial de a = " + a);
+        System.out.println("Valor inicial de b = " + b);
+         System.out.println("Incremento: a++");
+         a++;
+         System.out.println("Decremento: b--");
+         b--;
+         System.out.println("Agora a = " +a);
+         System.out.println("Agoara b= " +b);
+    }
+    
+}
